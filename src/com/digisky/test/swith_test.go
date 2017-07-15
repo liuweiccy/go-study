4e895f055071c9c63264e78dd0f28b842872cb70
liuweiccy <liuweiccy@qq.com>
2017-07-15 23:36:38 +0800

    学习goroutine

---
+++ src/com/digisky/test/swith_test.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"testing"
+	"fmt"
+	"time"
+)
+
+func TestSwitch(t *testing.T)  {
+	i := 2
+	fmt.Print("Write", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	s := time.Now()
+	switch {
+	case s.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch s := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Println("Don't konw type ", s)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
+
+func TestGoRoutine(t *testing.T)  {
+	f("direct")
+
+	go f("goroutine")
+
+	go func(s string) {
+		fmt.Println(s)
+	}("going")
+
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+}
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
