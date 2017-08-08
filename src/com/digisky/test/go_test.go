69f365b0a151f1e875480a969217ec4926c7a306
liuweiccy <liuweiccy@gmail.com>
2017-08-08 23:04:37 +0800

    tcp server

--- src/com/digisky/test/go_test.go
+++ src/com/digisky/test/go_test.go
@@ -5,6 +5,7 @@ import (
 	"runtime"
 	"testing"
 	"time"
+	"net"
 )
 
 func TestGo1(t *testing.T) {
@@ -179,4 +180,34 @@ func processChan(in <- chan int, out chan <- string)  {
 		out <- result
 	}
 }
+
+func TestGo11(t *testing.T)  {
+	fmt.Println("Starting the server ...")
+	listener,err := net.Listen("tcp", "localhost:50000")
+	if err != nil {
+		fmt.Println("Error listening", err.Error())
+		return
+	}
+	for {
+		conn,err := listener.Accept()
+		if err!=nil{
+			fmt.Println("Error accepting", err.Error())
+			return
+		}
+		go doServerStuff(conn)
+	}
+}
+func doServerStuff(conn net.Conn) {
+	for {
+		buf := make([]byte, 512)
+		_,err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading", err.Error())
+			return
+		}
+		fmt.Println("Received data:", string(buf))
+	}
+}
+
+
 
